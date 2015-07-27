2ae217b094af7d9dfba2b59564d6bc8cba4fb266
rattanakone <rattanakone@users.noreply.github.com>
2015-07-27 00:04:41 -0400

    completed requirements

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
-/* Begin by adding your on ready handler here, and then create the
-   rest of your functions inside the on ready handler.
 
-   (Note that you do not need to manually call Bootstrap functions in
-   your Javascript because Bootstrap will automatically recognize your
-   HTML structures and invoke the proper JS code accordingly. Be sure
-   to reference the Bootstrap documentation.)
-*/
+var map = L.map('map').setView([46.844, -121.717], 13); //invoke the Leaflet.js library to draw a map in `#map-container` div
 
-// TODO: Inside of your on ready handler, invoke the Leaflet.js library
-// to draw a map in your `#map-container` div.
+var satView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //satellite layer of the map
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'soumontha.mplae266',
+    accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
+}).addTo(map);
+ 
+var streetView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //street layer of the map
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'soumontha.mpl1b5n7',
+    accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
+}).addTo(map);
 
-// TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
-// tiles served through the MapQuest CDN. Consult this example to set up
-// the map tiles layers:
+var outdoorView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //outdoor layer of the map
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'soumontha.mpla94ea',
+    accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
+}).addTo(map);
 
+var layerViews = { //variable for map legend of the different layers
+	"Satellite" : satView,
+	"Street" : streetView,
+	"Outdoor" : outdoorView,
+};
 
-// TODO: Customize that Map to show markers with popups at no fewer than 3
-// interesting locations. (You'll need to figure out the latitude/longitude for
-// these locations using a mapping tool such as Google Maps.)
+var mapLegend = L.control.layers(layerViews).addTo(map); //leaflet library legend for users to view different layers of the map
 
+var fryingPan = L.popup() //a popup marker for glacier
+    .setLatLng([46.85238475973365, -121.68336868286133])
+	.setContent("I like the glacier's name.")
+    .openOn(map);
+
+var nationalPark = L.marker([46.85467392981981, -121.70628547668457]).addTo(map); //a marker for the national park 
+
+var circleGlacier = L.circle([46.84005678194953, -121.69169425964355], 500, { //a circle marker for glacier 
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5
+}).addTo(map);
