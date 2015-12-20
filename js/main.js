c457790baaf3a90b61ed9ed6238c238afc5039b9
rattanakone <rattanakone@users.noreply.github.com>
2015-12-19 19:45:16 -0800

    changed layout

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@ new WOW().init();
 
 var map = L.map('map').setView([46.844, -121.717], 13); //invoke the Leaflet.js library to draw a map in `#map-container` div
 
-var satView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //satellite layer of the map
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'soumontha.mplae266',
-    accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
-}).addTo(map);
- 
+
 var streetView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //street layer of the map
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
@@ -23,10 +17,19 @@ var outdoorView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.
     accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
 }).addTo(map);
 
+var satView = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', { //satellite layer of the map
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'soumontha.mplae266',
+    accessToken: 'pk.eyJ1Ijoic291bW9udGhhIiwiYSI6IjY5ZjFkMzVhNTgxMjY0Y2QxY2Q5MzI2NDgzOGEzNjRlIn0.sdDH-b1NZWXdunEkSZs_sQ'
+}).addTo(map);
+
+
 var layerViews = { //variable for map legend of the different layers
-	"Satellite" : satView,
+	
 	"Street" : streetView,
 	"Outdoor" : outdoorView,
+    "Satellite" : satView,
 };
 
 var mapLegend = L.control.layers(layerViews).addTo(map); //leaflet library legend for users to view different layers of the map
